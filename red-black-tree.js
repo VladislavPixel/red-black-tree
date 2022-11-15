50e5973062af1be7277a69e8dfd3a760ed4a2674
VladislavPixel <pixel.world7@mail.ru>
2022-11-15 17:39:32 +0300

    create methods for RedBlackTree

--- red-black-tree.js
+++ red-black-tree.js
@@ -4,6 +4,22 @@ class RedBlackTree {
 		this.length = 0;
 	}
 
+	isBrokeRule(parent, node) {
+		if (parent === null) {
+			return false;
+		}
+
+		return parent.isRed === node.isRed;
+	}
+
+	checkGrandson(grandson, parent, grandfather) {
+		const isLeftParent = grandfather.left === parent ? true : false;
+
+		const isLeftGrandson = parent.left === grandson ? true : false;
+
+		return { isExternalGrandson: isLeftParent === isLeftGrandson, isLeft: isLeftGrandson };
+	}
+
 	insert(value, key) {
 		const newNode = new Node(value, key);
 
@@ -12,20 +28,108 @@ class RedBlackTree {
 
 			this.root = newNode;
 
-		} else {
-			let current = this.root;
+			this.length++;
 
-			while(current !== null) {
-				const isTriggerColor = !current.isRed && current.left !== null && current.left.isRed && current.right !== null && current.right.isRed;
+			return this.length;
+		}
 
-				if (isTriggerColor) {
-					if (current !== this.root) {
-						current.isRed = true;
+		const nodes = [];
+
+		let grandfather = null;
+
+		let parent = null;
+
+		let current = this.root;
+
+		while(current !== null) {
+			const isTriggerColor = !current.isRed && current.left !== null && current.left.isRed && current.right !== null && current.right.isRed;
+
+			if (isTriggerColor) {
+				if (current !== this.root) {
+					current.isRed = true;
+				}
+
+				current.left.isRed = false;
+
+				current.right.isRed = false;
+
+				if (this.isBrokeRule(parent, current)) {
+					const { isExternalGrandson, isLeft } = this.checkGrandson(current, parent, grandfather);
+
+					if (isExternalGrandson) {
+						grandfather.isRed = !grandfather.isRed;
+
+						parent.isRed = !parent.isRed;
+
+						if (isLeft) {
+							this.ror(grandfather, parent, nodes);
+						} else {
+							this.rol();
+						}
+					} else {
+						// ДОДЕЛАТЬ двойные повороты
+					}
+				}
+
+			} else {
+				if (parent !== grandfather) {
+					const save = grandfather;
+
+					if (save !== null) {
+						nodes.push(save);
 					}
 
-					current.left.isRed = false;
+					grandfather = parent;
+				}
 
-					current.right.isRed = false;
+				parent = current;
+
+				if (key < current.key) {
+					current = current.left;
+
+					if (current === null) {
+						parent.left = newNode;
+					}
+
+				} else {
+					current = current.right;
+
+					if (current === null) {
+						parent.right = newNode;
+					}
+				}
+			}
+		}
+
+		if (this.isBrokeRule(parent, newNode)) {
+			const { isExternalGrandson, isLeft } = this.checkGrandson(newNode, parent, grandfather);
+
+			if (isExternalGrandson) {
+				grandfather.isRed = !grandfather.isRed;
+
+				parent.isRed = !parent.isRed;
+
+				if (isLeft) {
+					this.ror(grandfather, parent, nodes);
+
+				} else {
+					this.rol(grandfather, parent, nodes);
+				}
+
+			} else {
+				newNode.isRed = !newNode.isRed;
+
+				grandfather.isRed = !grandfather.isRed;
+
+				if (isLeft) {
+					this.rorSmall(grandfather, parent, newNode);
+
+					this.rol(grandfather, newNode, nodes);
+
+				} else {
+					this.rolSmall(grandfather, parent, newNode);
+
+					this.ror(grandfather, newNode, nodes);
 				}
 			}
 		}
@@ -35,19 +139,65 @@ class RedBlackTree {
 		return this.length;
 	}
 
-	ror() {
+	rorSmall(grandfather, parent, grandson) {
+		// if (grandfather.right === parent) {
+			
+		// } else {}
 
+		grandson.right = parent;
+
+		grandfather.right = grandson;
+
+		parent.left = grandson.left;
 	}
 
-	rol() {
+	rolSmall(grandfather, parent, grandson) {
+		// if (grandfather.left === parent) {
+		// } else {}
 
+		grandson.left = parent;
+
+		grandfather.left = grandson;
+
+		parent.right = grandson.right;
+	}
+
+	ror(grandfather, parent, nodes) {
+		if (nodes.length !== 0) {
+			const saveNode = nodes.pop();
+
+			// if (saveNode.left === grandfather) {
+			// } else {}
+
+			saveNode.left = parent;
+
+			grandfather.left = parent.right;
+
+			parent.right = grandfather;
+		} else {
+			grandfather.left = parent.right;
+
+			parent.right = grandfather;
+
+			this.root = parent;
+		}
+	}
+
+	rol(grandfather, parent, nodes) {
+		const saveNode = nodes.pop();
+
+		if (saveNode.left === grandfather) {
+			saveNode.left = parent;
+
+			grandfather.right = parent.left;
+
+			parent.left = grandfather;
+		} else {
+
+		}
 	}
 
 	find() {
-
-	}
-
-	remove() {
 
 	}
 }
---
+++ script.js
@@ -0,0 +1,44 @@
+const redBlackTree = new RedBlackTree();
+
+
+// ПОСЛЕ ВСТАВКИ =======================================================================
+// СЕТ ДЛЯ ВНЕШНЕГО ВНУКА С ПРАВЫМ ПОВОРОТОМ
+// redBlackTree.insert("color", 50);
+// redBlackTree.insert("home", 25);
+// redBlackTree.insert("damp", 75);
+// redBlackTree.insert("cerializer", 12);
+// redBlackTree.insert("poplol", 6);
+
+// СЕТ ДЛЯ ВНЕШНЕГО ВНУКА С ЛЕВЫМ ПОВОРОТОМ
+// redBlackTree.insert("color", 50);
+// redBlackTree.insert("home", 25);
+// redBlackTree.insert("damp", 75);
+// redBlackTree.insert("cerializer", 30);
+// redBlackTree.insert("poplol", 31);
+
+// СЕТ ДЛЯ ВНУТРЕННЕГО ВНУКА С ЛЕВЫМ + ПРАВЫМ ПОВОРОТАМИ
+// redBlackTree.insert("color", 50);
+// redBlackTree.insert("home", 25);
+// redBlackTree.insert("damp", 75);
+// redBlackTree.insert("cerializer", 12);
+// redBlackTree.insert("poplol", 18);
+
+// СЕТ ДЛЯ ВНУТРЕННЕГО ВНУКА С ПРАВЫМ + ЛЕВЫМ ПОВОРОТАМИ
+// redBlackTree.insert("color", 50);
+// redBlackTree.insert("home", 25);
+// redBlackTree.insert("damp", 75);
+// redBlackTree.insert("cerializer", 40);
+// redBlackTree.insert("poplol", 30);
+
+// ПРИ СПУСКЕ ===========================================================================
+// СЕТ ДЛЯ ВНЕШНЕГО НАРУШИТЕЛЯ
+redBlackTree.insert("color", 50);
+redBlackTree.insert("home", 25);
+redBlackTree.insert("damp", 75);
+redBlackTree.insert("cerializer", 12);
+redBlackTree.insert("poplol", 37);
+redBlackTree.insert("car", 6);
+redBlackTree.insert("suf", 18);
+redBlackTree.insert("fix", 3);
+
+console.log(redBlackTree);
